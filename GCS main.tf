d4b469cff3c009ea7ec5c7160da6b6952754ff8a
priyanka235262 <67422500+priyanka235262@users.noreply.github.com>
2024-12-03 11:37:05 +0530

    Update GCS main.tf

--- GCS main.tf
+++ GCS main.tf
@@ -25,4 +25,32 @@ location = "us-central1"
 lifecycle{
 prevent_storage = true
 
-#
+#GCS bucket with a service account having specific permissions:
+provider "google" {
+  project     = "your-project-id"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name          = "my-bucket-name"
+  location      = "us"
+  storage_class = "STANDARD"
+}
+
+resource "google_service_account" "my_service_account" {
+  account_id   = "my-service-account-id"
+  display_name = "My Service Account"
+}
+
+resource "google_service_account_key" "my_service_account_key" {
+  project     = google_service_account.my_service_account.project
+  service_account_id = google_service_account.my_service_account.account_id
+  private_key_type = "TYPE_RSA256"
+}
+
+resource "google_storage_bucket_acl" "my_bucket_acl" {
+  bucket = google_storage_bucket.my_bucket.name
+
+  role = "OWNER"
+  entity = google_service_account.my_service_account.email_address
+}

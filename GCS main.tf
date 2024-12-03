2c5080e5456a792924f0dc14a9af7ae64e240e69
priyanka235262 <67422500+priyanka235262@users.noreply.github.com>
2024-12-03 11:36:19 +0530

    Update GCS main.tf

--- GCS main.tf
+++ GCS main.tf
@@ -10,3 +10,19 @@ acl {
 ROLE = "READ_WRITE"
 }
 }
+#creating a storage bucket with lifecycle policy
+provider "google"{
+credentails = "file/path/to/your/services-keys.json"
+project = "my_project"
+region = "us-central1"
+uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "my_bucket"{
+name = "my_bucket"
+location = "us-central1"
+}
+lifecycle{
+prevent_storage = true
+
+#
